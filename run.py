ac8053469a86224dd3810e22a8a869d130850893
Francisco lucas (zeta) <lucasnasm@gmail.com>
2019-10-02 21:48:12 -0300

    Implementando funcoes processos

--- run.py
+++ run.py
@@ -4,6 +4,54 @@ import os
 def clearT():
   os.system('cls' if os.name == 'nt' else 'clear')
 
+def listProcess():
+  clearT()
+  for proc in ps.process_iter():
+    info = proc.as_dict(attrs=['pid', 'name', 'username', 'nice', 'cpu_times', 'status'])
+    print('Processo: {} | PID: {} | Nice: {} | User: {} | TempoExec: {} | Status: {} |'.format(info['name'], info['pid'], info['nice'], info['username'], info['cpu_times'][0], info['status']))
+  
+
+def listProcessUser():
+  clearT()
+  #print(ps.users()) #Era pra trazer os usuários
+  user = input("De qual usuário devo listar os processos? ")
+  print()
+  print('Lista de processos em execução do usuário: '+user+'\n')
+  for proc in ps.process_iter():
+    info = proc.as_dict(attrs=['pid', 'name', 'username', 'nice', 'cpu_times', 'status'])
+    if(info['username'] == user):
+      print('Processo: {} | PID: {} | Nice: {} | User: {} | TempoExec: {} | Status: {} |'.format(info['name'], info['pid'], info['nice'], info['username'], info['cpu_times'][0], info['status']))
+  input()
+
+def changePriorityProcess():
+  listProcess()
+  p = ps.Process(int(input("Informe o PID do processo: ")))
+  p.nice(int(input("Informe a nova prioridade -20 a 20: ")))
+  input()
+
+def changeStateProcess():
+  listProcess()
+  p = ps.Process(int(input("Informe o PID do processo: ")))
+  print(""" 
+1 - Bloquear;
+2 - Continuar ;
+3 - Executar;
+4 - Reiniciar;
+5 - Finalizar. """)
+  opcao = int(input())
+  if (opcao == 1):
+    p.suspend()
+  elif(opcao == 2):
+    p.resume()
+  elif(opcao == 3):
+    pass
+  elif(opcao == 4):
+    pass
+  elif(opcao == 5):
+    p.kill()
+
+  
+
 clearT()
 
 opcao = 1
@@ -12,33 +60,21 @@ while(opcao != 0):
   print()
   print("1 - Listar processos")
   print("2 - Listar processos de um usuário")
-  #print("3 - Alterar estado")
+  print("3 - Alterar estado")
   print("4 - Trocar prioridade de execução")
   print("0 - Sair")
 
   print()
   opcao = int(input("Informe a opção desejada: "))
   if(opcao == 1):
-    clearT()
-    for proc in ps.process_iter():
-      info = proc.as_dict(attrs=['pid', 'name', 'username', 'nice', 'cpu_times', 'status'])
-      print('Processo: {} | PID: {} | Nice: {} | User: {} | TempoExec: {} | Status: {} |'.format(info['name'], info['pid'], info['nice'], info['username'], info['cpu_times'][0], info['status']))
+    listProcess()
     input()
-
   elif(opcao == 2):
-    clearT()
-    #print(ps.users()) #Era pra trazer os usuários
-    user = input("De qual usuário devo listar os processos? ")
-    print()
-    print('Lista de processos em execução do usuário: '+user+'\n')
-    for proc in ps.process_iter():
-      info = proc.as_dict(attrs=['pid', 'name', 'username', 'nice', 'cpu_times', 'status'])
-      if(info['username'] == user):
-        print('Processo: {} | PID: {} | Nice: {} | User: {} | TempoExec: {} | Status: {} |'.format(info['name'], info['pid'], info['nice'], info['username'], info['cpu_times'][0], info['status']))
-    input()
-  
+    listProcessUser()
+  elif(opcao == 3):
+    changeStateProcess()    
   elif(opcao == 4):
-    pass
+    changePriorityProcess()
     #os.nice(1)
 
   clearT()
